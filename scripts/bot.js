db8cd0eaf8e04b77bfdaea694c1ff22f488d38fc
Hopson97 <mhopson@hotmail.co.uk>
2017-12-23 10:31:54 +0000

    prevent bot replying to oneself

--- scripts/bot.js
+++ scripts/bot.js
@@ -5,19 +5,35 @@ const client = new Discord.Client();
 const loginToken = Config.getToken();
 
 
-client.on('ready', () => {
-  console.log('I am ready!');
+client.on('ready', () => 
+{
+    console.log('I am ready!');
 });
 
 // Create an event listener for messages
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.channel.send('test');
-  }
+client.on('message', message => 
+{
+    if (message.author.id === "394055022436155392")
+    {
+        return;
+    }
+    if (message.content === 'ping') 
+    {
+        message.channel.send('pong');
+    }
   
-  if (message.content === 'pong') {
-    message.channel.send('ping');
-  }
+    if (message.content === 'pong') 
+    {
+        message.channel.send('ping');
+    }
+    /*
+    if (message.author.id == "115025985405059076") 
+    {
+        var thinking = client.emojis.find("name", "thinking");
+        message.react(thinking.id);
+    }
+    */
+    
 });
 
 // Log our bot in
