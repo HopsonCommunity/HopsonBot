2e89f84a2b83d355314cf0319d2210c13602a3ac
Hopson97 <mhopson@hotmail.co.uk>
2017-12-23 16:19:27 +0000

    Add complex replies
    Signed-off-by: Hopson97 <mhopson@hotmail.co.uk>

--- scripts/bot_replies.js
+++ scripts/bot_replies.js
@@ -9,38 +9,61 @@ module.exports =
     }
 }
 
-var simpleReplies = {
-    "source": "You can find my source code here: https://github.com/HopsonCommunity/HopsonBot !",
-};
+//Simple "struct" for replying to messages
+class Reply
+{
+    constructor(rep, description) 
+    {
+        this.rep = rep;
+        this.description = description;
+    }
+}
 
+
+//Dictionary of the different replies
+var simpleReplies = 
+{
+    "source": new Reply("You can find my source code here: https://github.com/HopsonCommunity/HopsonBot !", "Gives GitHub link of the bot's source code")
+}
+
+var complexReplies = 
+{
+    "help": new Reply(sendCommandList, "Shows a list of commands")
+}
+
+//Tries to reply to a message
 function reply(message, content) 
 {
     content = content.toLowerCase();
     if (content in simpleReplies)
     {
-        send(message, simpleReplies[content]);
+        send(message, simpleReplies[content].rep);
     }
-    else 
+    else if (content in complexReplies)
     {
-        if (content == "help")
-        {
-            sendCommandList(message);
-        }
+        complexReplies[content].rep(message);
     }
+}
+
+//Sends the list of commands
+function sendCommandList(message)
+{
+    let commands = "__**List of commands:**__\n";
+
+    for (rep in complexReplies)
+    {
+        commands = commands.concat("\n>" + rep + " - " + complexReplies[rep].description);
+    }
+
+    for (rep in simpleReplies)
+    {
+        commands = commands.concat("\n>" + rep + " - " + simpleReplies[rep].description);
+    }
+
+    send(message, commands);
 }
 
 function send(message, text)
 {
     message.channel.send(text);
-}
-
-function sendCommandList(message)
-{
-    let commands = "List of commands:";
-    for (rep in simpleReplies)
-    {
-        commands = commands.concat("\n>" + rep);
-        console.log(rep);
-    }
-    send(message, commands);
 }
