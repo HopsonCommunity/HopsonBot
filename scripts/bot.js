161e9efa024aaf13265518df3e9490cb54080f3f
Hopson97 <mhopson@hotmail.co.uk>
2017-12-23 10:17:36 +0000

    remove rood command

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,33 +1,22 @@
-/*
-  A ping pong bot, whenever you send "ping", it replies "pong".
-*/
-
-// Import the discord.js module
 const Discord = require('discord.js');
 const Config  = require('./config');
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
-
 const loginToken = Config.getToken();
 
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
+
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 // Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
   if (message.content === 'ping') {
-    // Send "pong" to the same channel
     message.channel.send('test');
   }
   
   if (message.content === 'pong') {
-    // Send "pong" to the same channel
-    message.channel.send('lol fuck you, pong');
+    message.channel.send('ping');
   }
 });
 
