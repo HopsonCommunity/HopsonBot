5d216e60214a92e80eb0d05b3104ed04831a12e6
Matthew Hopson <Hopson97@users.noreply.github.com>
2017-12-24 09:28:48 +0000

    Roles (#3)
    * begin role adding and removing
    
    * Improved output format
    
    * Better role modification validation
    
    * Finish role validation
    
    * Finish role modification

--- scripts/bot_replies.js
+++ scripts/bot_replies.js
@@ -17,17 +17,36 @@ class Reply
     }
 }
 
+//Roles the user is able to add/ remove to himself
+var avaliableRoles = [
+    "C++",
+    "Wot++",
+    "OpenGL",
+    "Linux",
+    "Windows",
+    "SFML",
+    "SDL",
+    "Java",
+    "C#",
+    "C-Language",
+    "Rust",
+    "Python",
+    "ASM"
+];
 
 //Dictionary of the different replies
 var simpleReplies =  {
-    "source": new Reply("You can find my source code here: https://github.com/HopsonCommunity/HopsonBot !", "Gives GitHub link of the bot's source code")
+    "source": new Reply("You can find my source code here: https://github.com/HopsonCommunity/HopsonBot !", "Gives GitHub link of the bot's source code"),
+    "roles":  new Reply("Roles:\n* " + avaliableRoles.join("\n* "), "Displays list of roles user is able to add and remove using the 'role' command")
 }
 
 //Complex replies call functions
 var complexReplies = {
-    "help":     new Reply (sendCommandList,     "Shows a list of commands"),
+    "role":     new Reply (modRole,             "Add/ Remove language roles. For a list of avaliable roles, say '>roles' Useage: `>role {add/ remove} {roleName} eg >role add C++ Python"),
     "echo":     new Reply (sendEcho,            "Echoes the first argument"),
     "ping":     new Reply (sendPing,            "Sends the current ping"),
+
+    "help":     new Reply (sendCommandList,     "Shows a list of commands") //Keep this one last
 }
 
 //Tries to reply to a message
@@ -59,16 +78,16 @@ function sendCommandList(message, args)
 
     function concat(rep, dict)
     {
-        commands = commands.concat("\n>" + rep + " - " + dict[rep].description);
+        commands = commands.concat("\n>" + rep + "\t-\t" + dict[rep].description);
+    }
+
+    for (rep in simpleReplies) {
+        concat(rep, simpleReplies);
     }
 
     for (rep in complexReplies) {
         concat(rep, complexReplies);
     } 
-
-    for (rep in simpleReplies) {
-        concat(rep, simpleReplies);
-    }
 
     send(message, commands);
 }
@@ -84,9 +103,74 @@ function sendEcho(message, args)
 
 function sendPing(message, args)
 {
+    send(message, "Ping: " + Math.round(message.client.ping).toString());
     send(message, "Ping: " + message.client.ping.toString());
 }
 
+function validateModRoles(message, args)
+{
+    //Check if there is a list number of args
+    if (args.length < 2) {
+        send(message, "You didn't provide me enough information. I need to know whether you want to add or remove roles, and the name of the roles.")
+        return false;
+    }   
+
+    //Check if  the modifier is valid
+    let modifer = args[0].toLowerCase();
+    if (modifer != "remove" && modifer != "add") {
+        send(message, "I need to know whether you want to add or remove roles.")
+        return false;
+    }
+
+    //Check if the role is actually in the list of avaliable roles
+    let languages = args.slice(1);
+    for (language of languages)  {
+        if (avaliableRoles.indexOf(language) == -1) {
+            send(message, "I do not recognise the " + language + " role, sorry. Make sure you have correct casing.");
+            return false;
+        }
+    }
+    return true;
+}
+
+function modRole(message, args)
+{
+    if (validateModRoles(message, args))
+    {
+        function getRoleList(rolesStrings) 
+        {
+            let roleList = []
+            for (r of rolesStrings) {
+                roleList.push(message.guild.roles.find("name", r));
+            }
+            return roleList;
+        }
+        //let role = message.guild.roles.find("name", "Test");
+        // message.member.addRole(role);
+
+        let modifer = args[0].toLowerCase();
+        let langs   = args.slice(1)
+        let roles   = getRoleList(langs);
+        let verb    = "";
+
+        if (modifer === "add") {
+            for (role of roles) {
+                message.member.addRole(role);
+            }
+            verb = "added";
+        }
+        else if (modifer === "remove") {
+            for (role of roles) {
+                message.member.removeRole(role);
+            }
+            verb = "removed";
+        }
+        let o = langs.length == 1 ? "role" : "roles";
+        let id = message.author.id.toString();
+        let output = "I have " + verb + " the following " + o + " to <@" + id + ">:\n* " + langs.join("\n* ");
+        send(message, output)
+    }
+}
 
 function send(message, text)
 {
