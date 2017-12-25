6f26b7a19b9e358f0670b3e5ba5df7197df114ac
Hopson97 <mhopson@hotmail.co.uk>
2017-12-25 22:24:58 +0000

    Allow the edit message event to work

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,11 +1,28 @@
-const Discord = require('discord.js');
-const Config  = require('./config');
-const Reply   = require('./bot_replies');
+const Discord   = require('discord.js');
+const Config    = require('./config');
+const Reply     = require('./bot_replies');
+const Bot       = require("./bot");
 
 const client        = new Discord.Client();
 const loginToken    = Config.getToken();
 const tryReply      = Reply.tryReply;
 
+module.exports = {
+    /*
+        Adds common data to a rich embeded message, and then sends it to a channel
+     */
+    sendRichEmbed: function(channel, richEmbed) 
+    {
+        richEmbed
+            .setColor(3447003)
+            .setAuthor(
+                client.user.username,
+                client.user.avatarURL)
+            .setFooter("Hopson Bot :)", client.user.avatarURL)
+            .setTimestamp();
+        channel.send(richEmbed);
+    }
+}
 
 client.on('ready', () => 
 {
@@ -25,45 +42,26 @@ client.on('message', message =>
 
 client.on('messageUpdate', (oldMessage, newMessage) => 
 {
+    //Commented out as this throws 
+    // if (!/\S/.test(value)) throw new RangeError('RichEmbed field values may not be empty.');
+    //:shrug:
     /*
     let memberName  = oldMessage.member.displayName;
     let channelName = oldMessage.channel.name;
     let botLog      = oldMessage.guild.channels.find('name', "bot_log");
 
-    //Send embed message
-    botLog.send({embed: {
-        color: 3447003,
-        author: {
-            name: client.user.username,
-            icon_url: client.user.avatarURL
-        },
-        title: "Message was updated in",
-        Fields: [  
-        {
-            name: "Channel",
-            value: channelName
-        },
-        {
-            name: "User",
-            value: memberName
-        },
-        {
-            name: "Old Message",
-            value: oldMessage
-        },
-        {
-            name: "New Message",
-            value: newMessage
-        }
-        ],
-        timestamp: new Date(),
-        footer: {
-            icon_url: client.user.avatarURL,
-            text: ":)"
-          }
-        }
-    });
-    */  
+    //Create the fields of the embed to send
+    embed = new Discord.RichEmbed()
+        .addField("__Channel__", channelName)
+        .addField("__Member__", memberName)
+        .addField("__Old Message__", oldMessage)
+        .addField("__New Message__", newMessage)
+        .addField("__Time Created__", oldMessage.createdTimestamp)
+        .addField("__Time Edited__", newMessage.createdTimestamp);
+
+    //Send it
+    Bot.sendRichEmbed(botLog, embed);
+    */
 });
 
 // Log our bot in
