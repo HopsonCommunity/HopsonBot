1362cba27a99841abfeef1d5f32644d7ebafc9ef
Hopson97 <mhopson@hotmail.co.uk>
2017-12-26 22:46:44 +0000

    Fix the map

--- hopson_bot/command_handler.js
+++ hopson_bot/command_handler.js
@@ -12,41 +12,45 @@ module.exports =
         let command = content[0].toLowerCase();
         let args    = content.slice(1);
 
+        console.log(content);
+        console.log();
         tryRespondToCommand(message, command, args);
     }
 }
 
-//Set up the commands
+//Maps to hold the two command types
 var simpleCommands = new Map();
-
-function addSimpleCommand(name, output, description) 
-{
-    simpleCommands.set(name, new Command(output, description, false));
-    console.log(`Simple command added!\nName: "${name}"\nOutput: "${output}"\nDescription: "${description}"\n`);
-
-}
 
 //Looks to see if the command sent is actually a command, and then responds to it
 function tryRespondToCommand(message, command, args) 
 {
-    console.log("Numaber of args " + args.length);
     console.log("Command sent: "   + command);
-    console.log("Map of commands");
-    console.log(simpleCommands);
     if (simpleCommands.has(command)) {
-        if (args.length > 0) return;
-        console.log("Action is: ");
-        console.log(typeof simpleCommands["source"].action);
-        console.log("\n");
-        console.log(typeof simpleCommands[command].action);
-        console.log("\n\n\n\n");
-        Bot.sendMessage(message.channel, simpleCommands[command].action);
+        if (args.length > 0) 
+            return;
+        Bot.sendMessage(message.channel, simpleCommands.get(command).action);
     }
 }
 
+
+//Add commands 
+function addSimpleCommand(name, output, description) 
+{
+    simpleCommands.set(name, new Command(output, description, false));
+    console.log(`Simple command added! \nName: "${name}" \nOutput: "${output}"  \nDescription:  "${description}"\n`);
+}
+
+addSimpleCommand(
+    "source",
+    "You can find my source code at https://github.com/HopsonCommunity/HopsonBot",
+    "Sends a link to the source code for this bot."
+);
+
+/*
 //Add commands 
 addSimpleCommand(
     "source",
     "You can find my source code at https://github.com/HopsonCommunity/HopsonBot",
     "Sends a link to the source code for this bot."
 );
+*/
--- hopson_bot/hopson_bot.js
+++ hopson_bot/hopson_bot.js
@@ -1,19 +1,20 @@
 
 const Discord       = require('discord.js');
 const Config        = require('./config');
-const EventHandler  = require("./event_handler")
 
 //Log into discord
 const client    = new Discord.Client();
 client.login(Config.getToken());
 
-//Start the event handler
-new EventHandler(client).run();
-
 module.exports = 
 {
     sendMessage : function(channel, message) 
     {
+        console.log(`Message sent by bot in channel "${channel.name}`);
         channel.send(message);
     }
 }
+
+//Start the event handler
+const EventHandler  = require("./event_handler")
+new EventHandler(client).run();
