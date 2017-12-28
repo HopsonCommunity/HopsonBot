05845e43749e90fd483eceabf0d0db7f1859f45b
Hopson97 <mhopson@hotmail.co.uk>
2017-12-28 01:33:36 +0000

    update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,21 @@
-node src/hopson_bot.js
-pause
+@echo off
+
+call :runBot
+
+:getInput
+    echo Bot has failed.
+    echo Options:
+    echo 1. Exit
+    echo 2. Run again
+    set /p option = What do you want to do [1, 2]? 
+    if %option% == 1:
+        call :exit
+    if %option% == 2:
+        call :runBot
+    
+:runBot
+    node src/hopson_bot.js
+    call :getInput
+
+:exit
+    pause
