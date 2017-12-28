5427a38925009457d1659b7f858db387431cdcbc
Hopson97 <mhopson@hotmail.co.uk>
2017-12-28 00:42:21 +0000

    Change CommandHandler to be a class

--- src/command_handler.js
+++ src/command_handler.js
@@ -5,10 +5,20 @@ const Roles     = require("../data/roles.json");
 const RoleMod   = require("./role_modifier");
 const Quiz      = require("./quiz")
 
-module.exports =
+module.exports = class CommandHandler 
 {
+    constructor() 
+    {
+        //Init some maps and list to hold data to be used by command handler
+        this.simpleCommands      = new Map();
+        this.functionCommands    = new Map();
+        this.roles               = Roles.roles;
+
+        this.initCommands();
+    }
+
     //Says it on the tin
-    handleCommand : function(message)
+    handleCommand (message)
     {
         let content = message.content.slice(1); //Remove the ">" from the message
 
@@ -17,117 +27,111 @@ module.exports =
         let command = content[0].toLowerCase();
         let args    = content.slice(1);
 
-        tryRespondToCommand(message, command, args);
-    }
-}
-
-//Init some maps and list to hold data to be used by command handler
-var simpleCommands      = new Map();
-var functionCommands    = new Map()
-var roles               = Roles.roles;
-
-//Returns true if the user is indeed an admin (has admin role)
-function isAdmin(user) 
-{
-    return user.roles.find("name", "Admins") != null;
-}
-
-//Looks to see if the command sent is actually a command, and then responds to it
-function tryRespondToCommand(message, command, args)
-{
-    if (isAdmin(message.member)) {
-        console.log("Sent by admin");
-    }
-    //Check to see if it a simple command sent
-    if (simpleCommands.has(command)) {
-        if (args.length > 0)
-            return;
-        Bot.sendMessage(message.channel, simpleCommands.get(command).action);
+        this.tryRespondToCommand(message, command, args);
     }
 
-    //Check to see if it is a function command sent
-    if  (functionCommands.has(command)) {
-        let commandHandle = functionCommands.get(command);
-        if (!commandHandle.acceptsArgs && args.length > 0){
+    //Returns true if the user is indeed an admin (has admin role)
+    isAdmin(user) 
+    {
+        return user.roles.find("name", "Admins") != null;
+    }
+
+    //Looks to see if the command sent is actually a command, and then responds to it
+    tryRespondToCommand(message, command, args)
+    {
+        if (this.isAdmin(message.member)) {
+            console.log("Sent by admin");
+        }
+        //Check to see if it a simple command sent
+        if (this.simpleCommands.has(command)) {
+            if (args.length > 0)
+                return;
+            Bot.sendMessage(message.channel, this.simpleCommands.get(command).action);
+        }
+
+        //Check to see if it is a function command sent
+        if  (this.functionCommands.has(command)) {
+            let commandHandle = this.functionCommands.get(command);
+            if (!commandHandle.acceptsArgs && args.length > 0){
+                return;
+            }
+            commandHandle.action(message, args);
+        }
+        else {
             return;
         }
-        commandHandle.action(message, args);
+        Bot.logMessage(`Command "${command}" sent in channel "${message.channel.name}" by "${message.member.displayName}"`)
     }
-    else {
-        return;
+
+    //Simple function replies
+    sendHelpList(message, args)
+    {
+        let output = "**__List of commands:__**\n\n";
+
+        function addOutput(m) {
+            m.forEach(function(val, key, map) {
+                output += `__**>${key}**__\n${val.description}\n\n`;
+            });
+        }
+        //Add in the simple commands to the final outputtted message
+        console.log("Yo");
+        addOutput(this.simpleCommands);
+        console.log("Yo2");
+        addOutput(this.functionCommands);
+        console.log("Yo3");
+        Bot.sendMessage(message.channel, output);
     }
-    Bot.logMessage(`Command "${command}" sent in channel "${message.channel.name}" by "${message.member.displayName}"`)
-}
 
-//Simple function replies
-function sendHelpList(message, args)
-{
-    let output = "**__List of commands:__**\n\n";
-
-    function addOutput(m) {
-        m.forEach(function(val, key, map) {
-            output += `__**>${key}**__\n${val.description}\n\n`;
-        });
+    //Adds simple "print" commands
+    addSimpleCommand(name, output, description)
+    {
+        this.simpleCommands.set(name, new Command(output, description, false));
+        Bot.logMessage(`Simple command added! \nName: "${name}"\nDescription:  "${description}"`);
     }
-    //Add in the simple commands to the final outputtted message
-    addOutput(simpleCommands);
-    addOutput(functionCommands);
-    Bot.sendMessage(message.channel, output);
+
+    //Adds commands which call a function
+    addFunctionCommand(name, func, description, acceptsArgs)
+    {
+        this.functionCommands.set(name, new Command(func, description, acceptsArgs));
+        Bot.logMessage(`Function command added! \nName: "${name}"\nDescription:  "${description}"\nArgs: ${acceptsArgs}`);
+    }
+
+    initCommands() 
+    {
+        //Add the "simple commands"
+        this.addSimpleCommand(
+            "source",
+            "You can find my source code at https://github.com/HopsonCommunity/HopsonBot",
+            "Sends a link to the source code for this bot."
+        );
+
+        this.addSimpleCommand(
+            "rolelist",
+            `**Roles you can add to yourself using the "__>role add <name>__" command:**\n> ${this.roles.join("\n> ")}.`,
+            "Displays list of roles you are able to add and remove."
+        );
+
+        //Add the "function commands"
+        this.addFunctionCommand(
+            "help",
+            this.sendHelpList.bind(this),
+            "Sends a list of commands.",
+            false
+        );
+
+
+        this.addFunctionCommand(
+            "role",
+            RoleMod.tryModifyRole,
+            "Allows the user to add or remove role(s) from '>rolelist'\nUseage: '>role add C++ Java'",
+            true
+        );
+
+        this.addFunctionCommand(
+            "quiz",
+            Quiz.handleQuizCommand,
+            "Starts or ends a quiz\nUseage: '>quiz start' '>quiz end'",
+            true
+        );
+    }
 }
-
-function handleQuizCommand()
-{
-
-}
-
-
-//Adds simple "print" commands
-function addSimpleCommand(name, output, description)
-{
-    simpleCommands.set(name, new Command(output, description, false));
-    Bot.logMessage(`Simple command added! \nName: "${name}"\nDescription:  "${description}"`);
-}
-
-//Adds commands which call a function
-function addFunctionCommand(name, func, description, acceptsArgs)
-{
-    functionCommands.set(name, new Command(func, description, acceptsArgs));
-    Bot.logMessage(`Function command added! \nName: "${name}"\nDescription:  "${description}"\nArgs: ${acceptsArgs}`);
-}
-
-
-//Add the "simple commands"
-addSimpleCommand(
-    "source",
-    "You can find my source code at https://github.com/HopsonCommunity/HopsonBot",
-    "Sends a link to the source code for this bot."
-);
-
-addSimpleCommand(
-    "rolelist",
-    `**Roles you can add to yourself using the "__>role add <name>__" command:**\n> ${roles.join("\n> ")}.`,
-    "Displays list of roles you are able to add and remove."
-);
-
-//Add the "function commands"
-addFunctionCommand(
-    "help",
-    sendHelpList,
-    "Sends a list of commands.",
-    false
-);
-
-
-addFunctionCommand(
-    "role",
-    RoleMod.tryModifyRole,
-    "Allows the user to add or remove role(s) from '>rolelist'\nUseage: '>role add C++ Java'",
-    true
-);
-
-addFunctionCommand(
-    "quiz",
-    Quiz.handleQuizCommand,
-    "Starts or ends a quiz\n Useage: '>quiz start' '>quiz end'",
-    true
-);
