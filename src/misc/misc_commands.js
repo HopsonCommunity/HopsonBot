e096d3609298064049eb3ce13112d4176af3ba7f
Adrian <bag4life5@gmail.com>
2017-12-28 15:16:08 +0000

    Reorganized Misc Command Structure (#11)
    * 8ball Command
    
    * Reorganized the Misc Commands

--- src/misc/misc_commands.js
+++ src/misc/misc_commands.js
@@ -1,34 +1,13 @@
 const Bot = require("../hopson_bot")
-const Util = require('./util')
 
-// Contains the possible results for the 8ball
-var ball_results = ["Yes.", "Reply hazy, try again.", "Without a doubt.",
-		    "My sources say no.", "As I see it, yes.", "You may rely on it.",
-		    "Concentrate and ask again.", "Outlook not so good",
-		    "It is decidedly so.", "Better not tell you now.",
-		    "Very doubtful.", "Yes - definitely.", "It is certain.",
-		    "Cannot predict now.", "Most likely.", "Ask again later.",
-		    "My reply is no.", "Outlook good.", "Don't count on it.",
-		    "Are you kidding?", "Probably.", "Yes, in due time.", "Go away.",
-		    "Hopson is more likely to put out a video than that"];
+const eight_ball = require("./eight_ball");
 
 module.exports =
 {
-    eightBall: function(message, args)
-    {
-		// Make sure the field is not empty
-		console.log("Args[0] " + args[0]);
-		if (args[0] === undefined) {
-			Bot.sendMessage(message.channel, "No question given");
-			return;
-		}
-		
-		// Get result
-		var result = Util.getRandomInt(0, ball_results.length);
-
-		// Print the result to the user
-		console.log("result: " + result);
-		Bot.sendMessage(message.channel, `🎱 The Magic 8-Ball says: "${ball_results[result]}"🎱`);
+    eightBall : function(message, args) {
+	eight_ball.main(message, args);
     }
 }
+
    
+
