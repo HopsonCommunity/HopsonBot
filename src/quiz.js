5fea1cad2480fbcdbc3acdf14b3afa467ae11a1a
Hopson97 <mhopson@hotmail.co.uk>
2017-12-29 19:08:21 +0000

    Quiz skipping works for a single user

--- src/quiz.js
+++ src/quiz.js
@@ -19,52 +19,87 @@ class Question
     }
 }
 
+//struct for holding data about a current person participating in the quiz
+class QuizUser
+{
+    constructor()
+    {
+        this.score      = 1;
+        this.hasSkipped = false;
+    }
+}
+
 
 //Handles a single session of a quiz
 class QuizSession 
 {
     constructor(channel) 
     {
-        this.skips      = 0;
+        this.skipVotes  = 0;
         this.channel    = channel;     //The channel the quiz is currently in
         this.question   = this.getQuestion();
-        this.scores     = new Map();
+        this.users      = new Map();
         this.printQuestion();
         Bot.sendMessage(this.channel, "Quiz has begun!");
     }
 
+    //Sends a message to the channel where the quiz is active
     sendMessage(message)
     {
+        message.setColor(0x28abed)
         Bot.sendMessage(this.channel, message);
     }
 
+    //Initiates the next question for the quiz
     nextQuestion() 
     {
-        this.skips = 0;
+        this.users.forEach(function(user, key, map) {
+            user.hasSkipped = false;
+        }.bind(this));
+        this.skipVotes = 0;
         this.question = this.getQuestion();
         this.printQuestion();
     }
 
-    addSkip() 
+    //Adds a member to this quiz session if they have not yet been added
+    tryAddUser(member) 
     {
-        console.log("Skip registered");
-        this.skips++;
-        let skipsNeeded = Math.floor(this.scores.size / 2);
-        if (this.skips >= this.skipsNeeded) {
-            this.sendMessage(new Discord.RichEmbed()
-            .setTitle("Question Skipped!")
-            .addField("Question", this.question.question)
-            .addField("Answer", this.question.answer));
-            this.nextQuestion();
-        }
-        else {
-            this.sendMessage(new Discord.RichEmbed()
-            .setTitle("Skip attempt registered...")
-            .addField("Current Skip Votes", this.skips.toString(), true)
-            .addField("Votes Needed", skipsNeeded.toString(), true));
+        if(!this.users.has(member) && this.users.size < 23) {
+            this.users.set(member, new QuizUser());
         }
     }
 
+    //Adds a skip to the vote, if it exceeds a certain number then the question is skipped, and a new question is preseneted
+    addSkip(member) 
+    {
+        this.tryAddUser(member);
+        //A user cannot skip twice
+        if(this.users.get(member).hasSkipped) {
+            this.sendMessage(new Discord.RichEmbed()
+                .setTitle(`You cannot vote to skip twice, ${member.displayName}, sorry.`))
+        }
+        else {
+            this.users.get(member).hasSkipped = true
+            this.skipVotes++;
+            let skipsNeeded = Math.floor(this.users.size / 2);
+            //Check if the current question is able to be skipped
+            if (this.skipVotes >= skipsNeeded) {
+                this.sendMessage(new Discord.RichEmbed()
+                .setTitle("Question Skipped!")
+                .addField("Question", this.question.question)
+                .addField("Answer", this.question.answer));
+                this.nextQuestion();
+            }
+            else {
+                this.sendMessage(new Discord.RichEmbed()
+                .setTitle("Skip Vote Registered")
+                .addField("Current Votes", this.skipVotes.toString(), true)
+                .addField("Votes Needed", skipsNeeded.toString(), true));
+            }
+        }
+    }
+
+    //Gets a random question from the main JSON file
     getQuestion() 
     {
         let inFile = JSONFile.readFileSync(questionsFile);
@@ -73,6 +108,7 @@ class QuizSession
         return question;
     }
 
+    //Displays the question
     printQuestion()
     {
         this.sendMessage(new Discord.RichEmbed()
@@ -82,49 +118,46 @@ class QuizSession
             .addField("**Question Author**", `<@${this.question.author}>`));
     }
 
+    //Ends the quiz
     endQuiz()
     {
         this.sendMessage("Quiz has ended!");
         this.outputScores("Final");
     }
 
-    addPointTo(user)
+    //Adds a point to a user, usually because they have answered a question correctly
+    addPointTo(member)
     {
-        if(this.scores.has(user)) {
-            let score = this.scores.get(user);
-            this.scores.set(user, score + 1);
-        }
-        else if (this.scores.size < 23){
-            this.scores.set(user, 1);
-        }
+        this.tryAddUser(member);
+        let quizMember = this.users.get(member);
+        quizMember.score++;
     }
 
+    //Sends a message saying the current scores of everyone
     outputScores(title) 
     {
         let output = new Discord.RichEmbed()
             .setTitle(title + " Scores");
 
-        this.scores.forEach(function(score, user, map) {
+        this.users.forEach(function(person, user, map) {
             //output.addField(`<@${user.id}>`, score.toString(), true);
-            output.addField(user.displayName, score.toString(), true);
+            output.addField(user.displayName, person.score.toString(), true);
         });
 
         this.sendMessage(output);
     }
         
-
-    submitAnswer(user, answer)
+    //Submits an answer to current question, and if the question is answered correctly, then it gets the next question
+    submitAnswer(member, answer)
     {
         if (answer.toLowerCase() == this.question.answer.toLowerCase()) {
             this.sendMessage(new Discord.RichEmbed()
                 .setTitle("Answered sucessfully!")
-                .addField("Answered By", `<@${user.id}>`));
-            this.addPointTo(user);
+                .addField("Answered By", `<@${member.id}>`));
+            this.addPointTo(member);
             this.outputScores("Current");
             this.nextQuestion();
-            return true;
         }
-        return false;
     }
 }
 
@@ -281,9 +314,8 @@ module.exports = class Quiz
     {
         if (!this.quizActive) return;
         if (this.session.channel != message.channel) return;
-        if (this.session.submitAnswer(message.member, answer)) {
-            //this.endQuiz();
-        }
+
+        this.session.submitAnswer(message.member, answer);
     }
 
     printQuestion() 
@@ -293,10 +325,10 @@ module.exports = class Quiz
         }
     }
 
-    trySkip() 
+    trySkip(member) 
     {
         if(this.quizActive) {
-            this.session.addSkip();
+            this.session.addSkip(member);
         }
     }
 }
