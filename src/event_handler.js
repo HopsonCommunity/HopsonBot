5231953b593c5487a94775008efba9ebc1a2844b
Hopson97 <mhopson@hotmail.co.uk>
2017-12-30 00:08:41 +0000

    Add const modifer to "requires" in event_handler.js

--- src/event_handler.js
+++ src/event_handler.js
@@ -1,7 +1,7 @@
-Bot             = require("./hopson_bot");
-CommandHandler  = require("./command_handler");
-Quiz            = require("./quiz")
-Discord         = require('discord.js')
+const Bot             = require("./hopson_bot");
+const CommandHandler  = require("./command_handler");
+const Quiz            = require("./quiz")
+const Discord         = require('discord.js')
 
 //Main class for the bot, which does what it says on the tin
 module.exports = class EventHandler
