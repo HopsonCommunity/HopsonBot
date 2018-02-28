5207d053f5cb2d76ee785e31742925408982ab3c
Hopson97 <mhopson@hotmail.co.uk>
2018-02-28 06:35:01 +0000

    Improve date format for new members

--- src/member_join.js
+++ src/member_join.js
@@ -1,6 +1,7 @@
 const Config    = require("../data/config.json");
 const Bot       = require ("./hopson_bot");
 const Discord   = require('discord.js')
+const dateFormat  = require('dateformat');
 
 var exp = module.exports =
 {
@@ -9,14 +10,17 @@ var exp = module.exports =
         let channelName = Config.memberJoinChannel[member.guild.id];
         let channel     = Bot.getClient().channels.find("name", channelName);
 
-        let join = member.joinedAt;
-        let creation = member.user.createdAt;
+        let join        = dateFormat(member.joinedAt, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+        let creation    = dateFormat(member.user.createdAt, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+
+       // let imgURL = member.user.displayAvatarURL;
 
         Bot.sendMessage(channel, new Discord.RichEmbed()
             .setTitle("User Join")
+            //.setImage("Avatar", imgURL)
             .addField("**Name**", `<@${member.user.id}>`)
-            .addField("**Join Date**", join.toISOString().substring(0, 10))
-            .addField("**Account Create Data**", creation.toISOString().substring(0, 10))
+            .addField("**Join Date**", join)
+            .addField("**Account Create Data**", creation)
         );
     }
 }
