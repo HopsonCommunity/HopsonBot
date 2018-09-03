978f334d851b851617e4e6edad792ea8c94c24a5
Hopson97 <mhopson@hotmail.co.uk>
2018-09-03 11:58:01 +0100

    make it so intro role is added to new people and removed from old peeps

--- src/member_join.js
+++ src/member_join.js
@@ -7,26 +7,34 @@ var exp = module.exports =
 {
     handleJoin : function(member) 
     {
-        let channelName = Config.memberJoinChannel;
-        let channel     = Bot.getClient().channels.find("name", channelName);
+        checkAccountAge(member);
 
-        let join        = dateFormat(member.joinedAt, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-        let creation    = dateFormat(member.user.createdAt, "dddd, mmmm dS, yyyy, h:MM:ss TT");
-        let diff        = getTimeDifference(member.joinedAt, member.user.createdAt);
+        let newMemberRole = member.guild.roles.find('name', Config.newMemberRole);
+        member.addRole(newMemberRole);
+    }
+}
 
-        Bot.sendMessage(channel, new Discord.RichEmbed()
-            .setTitle("User Join")
-            .addField("**Name**", `<@${member.user.id}>`)
-            .addField("**Account Create Data**", creation)
-            .addField("**Join Date**", join)
-            .addField("**Time Between Create and Join (est)**", diff.regularDiff)
-            .addField("**Milliseconds Difference**", diff.unixTimeDiff)
-        );
+function checkAccountAge(member) 
+{
+    let channelName = Config.memberJoinChannel;
+    let channel     = Bot.getClient().channels.find("name", channelName);
 
-        //Notify Admins if account is less than 2 days old
-        if (diff.notify) {
-            Bot.sendMessage(channel, `<@&293440127601082368>`);
-        }
+    let join        = dateFormat(member.joinedAt, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+    let creation    = dateFormat(member.user.createdAt, "dddd, mmmm dS, yyyy, h:MM:ss TT");
+    let diff        = getTimeDifference(member.joinedAt, member.user.createdAt);
+
+    Bot.sendMessage(channel, new Discord.RichEmbed()
+        .setTitle("User Join")
+        .addField("**Name**", `<@${member.user.id}>`)
+        .addField("**Account Create Data**", creation)
+        .addField("**Join Date**", join)
+        .addField("**Time Between Create and Join (est)**", diff.regularDiff)
+        .addField("**Milliseconds Difference**", diff.unixTimeDiff)
+    );
+
+    //Notify Admins if account is less than 2 days old
+    if (diff.notify) {
+        Bot.sendMessage(channel, `<@&293440127601082368>`);
     }
 }
 
