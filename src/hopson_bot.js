189486b69685c5b08a0fe23dedf9587c3ea407d7
Hopson97 <mhopson@hotmail.co.uk>
2018-09-15 10:02:15 +0100

    Create hopson bot class to replace event handler class

---
+++ src/hopson_bot.js
@@ -0,0 +1,56 @@
+const Discord         = require("discord.js");
+const Config          = require("../data/config.json");
+
+module.exports = class HopsonBot {
+    constructor(client) {
+        this.client = client;
+    }
+
+    runBot() {
+        //Event for when the bot starts
+        this.client.on("ready", () => {
+            console.log("Client has logged in to server");
+            this.client.user.setPresence({game: {name : "Type >help"}})
+                .then(console.log)
+                .catch(console.error);
+        });
+
+        //Event for when bot is dissconnected
+        this.client.on("disconnect", (event) => {
+            console.log(`Client has closed with status code ${event.code} and reason ${event.reason}`)
+        });  
+
+        //Event for messages sent to any of the discord channels
+        this.client.on("message", (message) => {
+            //this.handleMessage(message);
+        });
+
+        this.client.on("messageDelete", (message) => {/*
+            for (var blacklistedChannel of Config.logBlacklistedChannels) {
+                if (message.channel.id == blacklistedChannel) {
+                    return;
+                }
+            }
+            
+            this.handleDelete(message);*/
+        });
+
+        this.client.on("messageUpdate", (oldMessage, newMessage) => {/*
+            for (var blacklistedChannel of Config.logBlacklistedChannels) {
+                if (oldMessage.channel.id == blacklistedChannel) {
+                    return;
+                }
+            }
+
+            this.handleEdit(oldMessage, newMessage);*/
+        });
+
+        this.client.on("guildMemberAdd", (member) => {
+           // MemberJoin.handleJoin(member);
+        });
+
+        this.client.on("userUpdate", (oldUser, newUser) => {
+            //this.handleUserUpdate(oldUser, newUser);
+        });
+    }
+}
