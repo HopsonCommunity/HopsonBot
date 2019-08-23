37991ec3cf9d0e3be60b8f876c0be228fcd60427
Hopson97 <mhopson@hotmail.co.uk>
2019-08-23 20:31:27 +0100

    Add comments to mock guild member

--- test/discord_mocks/mock_guild_member.js
+++ test/discord_mocks/mock_guild_member.js
@@ -1,13 +1,25 @@
 module.exports = class {
+    /**
+     * Creates a mock guild member
+     */
     constructor() {
         this.roles = [];
     }
 
+    /**
+     * Adds a mock role to the user
+     * @param {MockRole} role The role to add to the user
+     */
     addRole(role) {
         this.roles.push(role);
     }
 
+    /**
+     * Adds a mock role to the user
+     * @param {MockRole} role The role to add to the user
+     */
     removeRole(role) {
-        this.roles.pop(role);
+        const index = this.roles.indexOf(role);
+        if (index !== -1) this.roles.splice(index, 1);
     }
 }
