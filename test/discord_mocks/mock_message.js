d2c41a931b93bc17272e85420bae849e165b0e71
Hopson97 <mhopson@hotmail.co.uk>
2019-08-23 20:33:20 +0100

    Add docs to mock message

--- test/discord_mocks/mock_message.js
+++ test/discord_mocks/mock_message.js
@@ -2,6 +2,13 @@ const MockGuildMember = require('./mock_guild_member')
 const MockUser = require('./mock_user')
 
 module.exports = class {
+    /**
+     * Creates a mock message
+     * @param {String} content The content of a message
+     * @param {MockChannel} channel The channel to send the message to
+     * @param {MockMember} member The member who sent the message
+     * @param {MockUser} author The user who sent the message
+     */
     constructor(content, channel, member, author) {
         this.content = content;
         this.author = author;
@@ -10,6 +17,10 @@ module.exports = class {
         this.reactions = [];
     }
 
+    /**
+     * Reacts to the message with an emoji
+     * @param {Char} reaction The emoji to react with
+     */
     react(reaction) {
         this.reactions.push(reaction);
     }
